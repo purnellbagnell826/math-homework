d26bfcc06c2aff6e9aae072b6c2a049f171c5076
purnellbagnell826 <purnellbagnell826@gmail.com>
2025-03-25 02:20:46 +0000

    Add 18.js

---
+++ src/18.js
@@ -0,0 +1,9 @@
+function calculateArea() {
+    const width = 10;
+    const height = 5;
+
+    let area = width * height;
+    console.log(`The area of the rectangle is: ${area}`);
+}
+
+calculateArea();
