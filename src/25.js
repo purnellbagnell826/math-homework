f95941a6454245d3c88399ead6f864943af1a3f0
purnellbagnell826 <purnellbagnell826@gmail.com>
2025-04-05 06:22:03 +0000

    Add 25.js

---
+++ src/25.js
@@ -0,0 +1,11 @@
+function square(x) {
+  return x * x;
+}
+
+function cube(x) {
+  return x ** 3;
+}
+
+// Example usage: 
+console.log(square(5)); // Output: 25
+console.log(cube(7));   // Output: 343
